a2161be935d0e5a0c1b695dd219d941cb5bfd8e3
Sertaç Karahoda <skarahoda@sabanciuniv.edu>
2021-09-09 09:20:58 +0300

    Add solution of the fifth question

---
+++ src/question5.test.ts
@@ -0,0 +1,19 @@
+import { smallestMultiple } from './question5';
+
+describe('smallestMultiple', () => {
+  it.each`
+        max    | expected
+        ${2}   | ${2}
+        ${3}   | ${6}
+        ${4}   | ${12}
+        ${5}   | ${60}
+        ${6}   | ${60}
+        ${7}   | ${420}
+        ${8}   | ${840}
+        ${9}   | ${2520}
+        ${10}  | ${2520}
+        ${20}  | ${232792560}
+    `('should return $expected when the max is $max and', ({ max, expected }) => {
+    expect(smallestMultiple(max)).toBe(expected);
+  });
+});
---
+++ src/question5.ts
@@ -0,0 +1,12 @@
+export const smallestMultiple = (max: number) => {
+  let result = 2;
+  for (let i = 3; i <= max; i += 1) {
+    const mod = result % i;
+    if (mod !== 0 && i % mod === 0) {
+      result *= (i / mod);
+    } else if (mod !== 0) {
+      result *= i;
+    }
+  }
+  return result;
+};
